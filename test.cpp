a1e3bf89e85cf33b3dc763990306a823861e0f5f
icad08 <irsyadrm08@gmail.com>
2024-04-25 15:57:22 +0700

    case 3, 4, 5

--- test.cpp
+++ test.cpp
@@ -164,6 +164,20 @@ void searchData(){
                 else
                     cout << "Data tidak ditemukan" << endl;
                 break;
+                  case 3 :
+                traverse();
+                break;
+            case 4 :
+                searchData();
+                break;
+            case 5 :
+                break;
+            default : 
+                cout << "Pilihan tidak ada" << endl;
+                break;
+            }
+        }
+
 
 
 
