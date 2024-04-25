64eed4c4395e4edca80d440bae9f3668ca70737a
icad08 <irsyadrm08@gmail.com>
2024-04-25 15:52:15 +0700

    menambahkan void

--- test.cpp
+++ test.cpp
@@ -88,7 +88,14 @@ bool listEmpty() {
     else
         return false;
 }
-
+void traverse(){
+    if (listEmpty()){
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+}
 
 
 
