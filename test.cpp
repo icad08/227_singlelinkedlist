27014c0bb97187449cc59c803f8360215c9ca5c6
icad08 <irsyadrm08@gmail.com>
2024-04-25 15:53:48 +0700

    menambahkan else

--- test.cpp
+++ test.cpp
@@ -95,7 +95,17 @@ void traverse(){
         system("cls");
         return;
     }
+
+else {
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << "NIM : " << currentNode -> noMhs << ", Nama : " << currentNode -> name << endl;
+            currentNode = currentNode -> next;
+        }
+    }
 }
+
+
 
 
 
