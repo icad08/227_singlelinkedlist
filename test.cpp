c3a875b8174db2742c37ec94e62ef05f5fe5bc7f
icad08 <irsyadrm08@gmail.com>
2024-04-25 15:44:58 +0700

    nenambahkan perulangan if

--- test.cpp
+++ test.cpp
@@ -19,6 +19,22 @@ void addNode() {
     nodeBaru -> noMhs = nim;
     nodeBaru -> name = nama;
 
+     if (START == NULL || nim <= START -> noMhs){
+        if (START != NULL && nim == START -> noMhs)
+        {
+            cout << "NIM sudah ada " << endl;
+            return;
+        }
+
+        nodeBaru -> next = START;
+        START = nodeBaru;
+        return;
+    }
+
+    Node* previous = START;
+    Node* current = START;
+
+
 
 int main()
 {
